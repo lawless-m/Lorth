212829c3be0bd7316b2e222999db5c9bfef18edf
lawless-m <maht0x0r@xsmail.com>
2017-02-15 13:19:40 -0500

    This language is cute

--- TIL.lua
+++ TIL.lua
@@ -1,13 +1,28 @@
+-- Simple List
+
+List = function()
+	local list = {n=0}
+	list["append"] = function(v) list.n = list.n+1; list[list.n] = v; end
+	list["tostring"] = 
+		function()
+			local str = ""
+			for i = 1,list.n do
+				str = str .. i .. ":" .. list[i] .. "\n"
+			end
+			return str
+		end
+	end
+
 -- STACKS
 -- ok we will need stacks, here we go
 
-newStack = function()
-	local stk = {depth=0}
+Stack = function()
+	local stk = List()
 	stk["top"] = 
 		function()
 			local v
-			if stk.depth > 0 then
-				v = stk[stk.depth]
+			if stk.n > 0 then
+				v = stk[stk.n]
 			else
 				warn("Stack underflow")
 				v = nil
@@ -18,20 +33,45 @@ newStack = function()
 	stk["pop"] = 
 		function()
 			local v = stk.top()
-			if stk.depth > 0 then
-				stk[stk.depth] = nil
-				stk.depth = stk.depth - 1
+			if stk.n > 0 then
+				stk[stk.n] = nil
+				stk.n = stk.n - 1
 			end
 			return v
 		end
-	
+
 	stk["push"] = 
 		function(v) 
-			stk.depth = stk.depth + 1; 
-			stk[stk.depth] = v 
+			stk.append(v)
 		end
-	
+
 	return stk
+end
+
+Word = function()
+	local word = {
+		name = "",
+		vocab = "",
+		code = nil
+		data = List()
+	}
+	return word
+end
+
+Dict = function()
+	local dict = {
+		words = List()
+	}
+	dict["find"] = 
+		function(t)
+			for i = dict.words.n,-1,1
+				if dict.words.name == then
+					return i
+				end
+			end
+			return nil
+		end
+	return dict
 end
 
 DS = newStack()
